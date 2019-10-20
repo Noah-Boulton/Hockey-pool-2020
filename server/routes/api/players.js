92f8675b2eed89dc9a0a72e368bb660d46d22255
Noah Boulton <noah.boulton@gmail.com>
2019-10-20 14:36:34 -0400

    Trying not to break stuff

--- server/routes/api/players.js
+++ server/routes/api/players.js
@@ -41,51 +41,47 @@ async function updatePlayers() {
         const client = await mongodb.MongoClient.connect(process.env.DATABASE_CONNECTION_STRING, {useUnifiedTopology: true, useNewUrlParser: true });
         const playersdb = client.db('players').collection('players');
         const players = await playersdb.find({}).toArray();
+        players.forEach(async (player) => {
+            const query = {p_id : player.p_id};
+            const url = `https://statsapi.web.nhl.com/api/v1/people/${player.p_id}/stats?stats=statsSingleSeason`;
+            const res = await axios.get(url);
+            if(!res.err && res.data.stats[0].splits && res.data.stats[0].splits[0] && res.data.stats[0].splits[0].stat){
+                    
+                const data = res.data.stats[0].splits[0].stat;
+                var points = 0;
+                var pv = 1;
+                if(data.wins){
+                    // Goalie
+                    points = 2*data.wins;
+                    if(data.shutouts){
+                        points += data.shutouts;
+                    }
+                    pv = Math.round(((2*data.shutouts + data.wins)/(data.games+1))*1.8);
+                } else {
+                    // Skater
+                    points = data.goals + data.assists;
+                    pv = Math.round(((2*data.goals + data.assists)/(data.games+1))*1.5);
+                }
+                if(isNaN(points)){
+                    points = 0;
+                }
+                if(pv < 1){
+                    pv = 1;
+                }
+                if(pv > 4){
+                    pv = 4;
+                }
+                if(points != player.points){
+                    var newValues = { $set: {points: points, pv: pv} };
+                    await playersdb.updateOne(query, newValues, (err, res) => {
+                        if (err) throw err;
+                    });
+                }
+            }
+        client.close();
     } catch(e){
         console.error(e);
     }
-    players.forEach(async (player) => {
-        const query = {p_id : player.p_id};
-        const url = `https://statsapi.web.nhl.com/api/v1/people/${player.p_id}/stats?stats=statsSingleSeason`
-        try{
-            const res = await axios.get(url);
-        } catch(e){
-            console.error(e);
-        }
-        if(!res.err && res.data.stats[0].splits && res.data.stats[0].splits[0] && res.data.stats[0].splits[0].stat){
-                
-            const data = res.data.stats[0].splits[0].stat;
-            var points = 0;
-            var pv = 1;
-            if(data.wins){
-                // Goalie
-                points = 2*data.wins;
-                if(data.shutouts){
-                    points += data.shutouts;
-                }
-                pv = Math.round(((2*data.shutouts + data.wins)/(data.games+1))*1.8)
-            } else {
-                // Skater
-                points = data.goals + data.assists;
-                pv = Math.round(((2*data.goals + data.assists)/(data.games+1))*1.5)
-            }
-            if(isNaN(points)){
-                points = 0;
-            }
-            if(pv < 1){
-                pv = 1;
-            }
-            if(pv > 4){
-                pv = 4;
-            }
-            if(points != player.points){
-                var newValues = { $set: {points: points, pv: pv} };
-                await playersdb.updateOne(query, newValues, (err, res) => {
-                    if (err) throw err;
-                });
-            }
-        }
-        client.close();
     });
 }
 
