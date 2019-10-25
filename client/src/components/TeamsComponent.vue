99f6d3c53677d50f5fbfae97b1c3935f32ac3325
Noah Boulton <noah.boulton@gmail.com>
2019-10-25 15:46:36 -0400

    Changing picture crop for teams page

--- client/src/components/TeamsComponent.vue
+++ client/src/components/TeamsComponent.vue
@@ -11,7 +11,7 @@
           <img v-if="team.sid == 'kid'" src="../../public/images/sid-the-kid.jpg" class="card__image" style="border-radius: 50%" width=150 alt="Sid the Kid">
           <img v-if="team.sid == 'man'" src="../../public/images/sid-the-man.jpeg" class="card__image" style="border-radius: 50%" width=150 alt="Sid the Man">
           <img v-if="team.sid == 'beast'" src="../../public/images/sid-the-beast.jpg" class="card__image" style="border-radius: 50%" width=150 alt="Sid the Beast">
-          <img v-if="team.sid == 'mo'" src="../../public/images/mo_8.jpg" class="card__image" width=150 style="border-radius: 40%" alt="Commissioner Mo">          
+          <img v-if="team.sid == 'mo'" src="../../public/images/mo_8.jpg" class="card__image" width=150 style="border-radius: 50%" alt="Commissioner Mo">          
           <div class="card__content">
             <h2 class="card__title">{{ `${team.name}` }}</h2>
             <h3 class="card__subtitle">{{ `${team.owner}` }}</h3>
