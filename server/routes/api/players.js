c37a191b9ca31b217ecd3b5e64d19ae925e92773
Noah Boulton <noah.boulton@gmail.com>
2019-11-30 21:58:00 -0500

    Add fight tracking:

--- server/routes/api/players.js
+++ server/routes/api/players.js
@@ -14,8 +14,9 @@ cron.schedule('0 * * * *', () => {
 });
 
 cron.schedule('0 8 * * *', () => {
-    console.log("Updating goalies");
-    updateGoaliePoints();
+    console.log("Updating goalies and fights");
+    await updateGoaliePoints();
+    await updateFights();
 });
 
 // Get Players
@@ -181,6 +182,64 @@ async function updateGoaliePoints(){
                     }
                 });
             });
+        });
+        client.close();
+    }catch(e){
+        console.error(e);
+    }
+}
+
+async function updateFights(){
+    try {
+        const client = await mongodb.MongoClient.connect(process.env.DATABASE_CONNECTION_STRING, {useUnifiedTopology: true, useNewUrlParser: true });
+        const playersdb = client.db('players').collection('players');
+        var players = await playersdb.find({}).toArray();
+
+        var d = new Date();
+        d.setDate(d.getDate() - 1); //Yesterday
+        const url = `https://statsapi.web.nhl.com/api/v1/schedule?date=${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+        const res = await axios.get(url).catch((error) => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+            return;
+        });
+        const games = res.data.dates[0].games;
+        //Get all the game links for the current day
+        games.forEach(async game => {
+            //Use each games scoring plays to check for goalie points 
+            const gameURL = `https://statsapi.web.nhl.com${game.link}`;
+            const gameres = await axios.get(gameURL).catch((error) => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                  } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                  } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                  }
+                return;
+            });
+            const gameData = gameres.data;
             const penaltyPlays = gameData.liveData.plays.penaltyPlays;
             penaltyPlays.forEach(play => {
                 const data = gameData.liveData.plays.allPlays[play];
