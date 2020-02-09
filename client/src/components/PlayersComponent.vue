04c13a71c3d687a11e38ad9cdca02a304d79f6a9
Noah Boulton <noah.boulton@gmail.com>
2020-02-09 16:19:57 -0500

    Add pages to players component:

--- client/src/components/PlayersComponent.vue
+++ client/src/components/PlayersComponent.vue
@@ -4,7 +4,7 @@
         <p class="error" v-if="error">{{ error }}</p>
         <div>
             <div >
-                <table class="players-table">
+                <table class="players-table" ref="table">
                   <thead>
                 <tr>
                     <th @click="sort('name')" class="sortable" v-bind:data-sort="nameSort">Player</th>
@@ -36,6 +36,12 @@
                 </table>
             </div>
         </div>
+        <div> 
+          <button v-if="pageNumber != 0" v-on:click="back" class="button button--players-back">Back</button>
+        </div>
+        <div>
+          <button v-if="pageNumber != pages" v-on:click="next" class="button button--players-next">Next</button>
+        </div>
     </div>
 </template>
 
@@ -47,6 +53,9 @@ export default {
   data() {
     return {
       players: [],
+      fullList: [],
+      pageNumber: 0,
+      pages: 1,
       error: '',
       currentSort:'team',
       currentSortDir:'desc',
@@ -62,113 +71,125 @@ export default {
   },
   async created() {
     try {
-      this.players = await PlayersService.getPlayers();
+      this.fullList = await PlayersService.getPlayers();
+      // this.players = this.players.slice(0,25);
+      this.pages = Math.floor(this.fullList.length / 25);
+      this.players = this.fullList.slice(0, 25);
     } catch(error) {
       this.error = error.message;
     }
   },
   methods:{
     sort:function(s) {
-    if(s === this.currentSort) {
-      this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
-    }
-    if(s == 'name'){
-      this.nameSort = this.currentSortDir;
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = '';
-    } else if(s == 'pos'){
-      this.nameSort = '';
-      this.posSort = this.currentSortDir;
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = '';
-    }else if(s == 'team'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = this.currentSortDir;
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = '';
-    }else if(s == 'pv'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = this.currentSortDir;
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = '';
-    }else if(s == 'points'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = this.currentSortDir;
-    }
-    else if(s == 'goals'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort=this.currentSortDir;
-      this.assistsSort='';
-      this.fightsSort='';
-      this.pointsSort = '';
-    }
-    else if(s == 'assists'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort=this.currentSortDir;
-      this.fightsSort='';
-      this.pointsSort = '';
-    }
-    else if(s == 'fights'){
-      this.nameSort = '';
-      this.posSort = '';
-      this.teamSort = '';
-      this.pvSort = '';
-      this.goalsSort='';
-      this.assistsSort='';
-      this.fightsSort=this.currentSortDir;
-      this.pointsSort = '';
-    }
-    this.currentSort = s;
+      if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+      }
+      if(s == 'name'){
+        this.nameSort = this.currentSortDir;
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = '';
+      } else if(s == 'pos'){
+        this.nameSort = '';
+        this.posSort = this.currentSortDir;
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = '';
+      }else if(s == 'team'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = this.currentSortDir;
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = '';
+      }else if(s == 'pv'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = this.currentSortDir;
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = '';
+      }else if(s == 'points'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = this.currentSortDir;
+      }
+      else if(s == 'goals'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort=this.currentSortDir;
+        this.assistsSort='';
+        this.fightsSort='';
+        this.pointsSort = '';
+      }
+      else if(s == 'assists'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort=this.currentSortDir;
+        this.fightsSort='';
+        this.pointsSort = '';
+      }
+      else if(s == 'fights'){
+        this.nameSort = '';
+        this.posSort = '';
+        this.teamSort = '';
+        this.pvSort = '';
+        this.goalsSort='';
+        this.assistsSort='';
+        this.fightsSort=this.currentSortDir;
+        this.pointsSort = '';
+      }
+      this.currentSort = s;
+    },
+    back: function() {
+      this.pageNumber--;
+      this.players = this.fullList.slice(this.pageNumber*25, this.pageNumber*25 + 25);
+    },
+    next: function() {
+      this.pageNumber++;
+      this.players = this.fullList.slice(this.pageNumber*25, this.pageNumber*25 + 25);
+      // this.$refs.table.refresh();
     }
   },
   watch:{
     currentSort:function() {
-      return this.players.sort((a,b) => {
+      this.players = this.fullList.sort((a,b) => {
         let modifier = 1;
       if(this.currentSortDir === 'desc') modifier = -1;
       if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
       if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
       return 0;
-      });
+      }).slice(this.pageNumber*25, this.pageNumber*25 + 25);
     },
     currentSortDir:function() {
-      return this.players.sort((a,b) => {
+      this.players = this.fullList.sort((a,b) => {
         let modifier = 1;
       if(this.currentSortDir === 'desc') modifier = -1;
       if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
       if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
       return 0;
-      });
+      }).slice(this.pageNumber*25, this.pageNumber*25 + 25);
     }
   }
 };
